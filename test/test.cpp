062d0529cd65238cd95700217b3c2837750caef4
naokishami <naokishami@gmail.com>
2020-07-22 19:01:05 +0900

    in progress

---
+++ test/test.cpp
@@ -0,0 +1,28 @@
+std::map<std::string, std::set<std::string> > follow = {
+        { "Rat20SU", { "$" } },
+        { "OptDeclList", { "{", /* id */ "if", "put", "get", "while" } },
+        { "DeclList", { "{", /* id */ "if", "put", "get", "while" } },
+        { "DeclListPrime", { "{", /* id */ "if", "put", "get", "while" } },
+        { "Declaration", { ";" } },
+        { "Qualifier", { /* id */ } },
+        { "StatementList", { "}", "$$" } },
+        { "Statement", { "{", "}", "$$", /* id */ "if", "put", "get", "while", "otherwise", "fi" } },
+        { "Compound", { "{", "}", "$$", /* id */ "if", "put", "get", "while", "otherwise", "fi" } },
+        { "Assign", { "{", "}", "$$", /* id */ "if", "put", "get", "while", "otherwise", "fi" } },
+        { "If", { "{", "}", "$$", /* id */ "if", "put", "get", "while", "otherwise", "fi" } },
+        { "Otherwise", { "fi" } },
+        { "Put", { "{", "}", "$$", /* id */ "if", "put", "get", "while", "otherwise", "fi" } },
+        { "Get", { "{", "}", "$$", /* id */ "if", "put", "get", "while", "otherwise", "fi" } },
+        { "While", { "{", "}", "$$", /* id */ "if", "put", "get", "while", "otherwise", "fi" } },
+        { "Condition", { ")" } },
+        { "Relop", { "-", /* id */ /* int */ "(", "true", "false" } },
+        { "Expression", { "-", /* id */ /* int */ "true", "false", "(", ")", ";" } },
+        { "Expression Prime", { ")", "==", "<", ">", ";" } },
+        { "Term", { "+", "-", /* id */ /* int */ "true", "false", "(", ")", ";" } },
+        { "Term Prime", { "+", "-", "e" } },
+        { "Factor", { "*", "/", "+", "-", /* id */ /* int */ "true", "false", "(", ")", ";" } },
+        { "Primary", { "*", "/", "+", "-", /* id */ /* int */ "true", "false", "(", ")", ";" } },
+        { "Empty", { "{", /* id */ /* int */ "if", "put", "get", "while", "fi", "+", "-", "true", "false",
+                "(", ")", ";" } }
+
+if (first["DeclList"].find(token->lexeme) != first["DeclList"].end()) {
